b130a6eaa7c88526331ac83075ed009880441ae4
Subbu_Gunti <subbubhashya@gmail.com>
2026-09-07 16:23:34 +0530

    MongoDB connected, User model created

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err.message));
+
 app.get('/', (req, res) => {
   res.json({ message: 'Secure Cloud File Vault API is running' });
 });
